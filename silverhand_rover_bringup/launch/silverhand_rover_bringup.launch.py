5eb2ad6670c53217177c82bd369fd98a195e2fe3
Sorokonog <roman@voltbro.com>
2026-03-28 14:36:20 +0300

    Integrate approved Silverhand rover model

--- silverhand_rover_bringup/launch/silverhand_rover_bringup.launch.py
+++ silverhand_rover_bringup/launch/silverhand_rover_bringup.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -37,7 +38,9 @@ def generate_launch_description():
             queue_len,
         ]
     )
-    robot_description = {"robot_description": robot_description_content}
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_content, value_type=str)
+    }
 
     robot_state_publisher = Node(
         package="robot_state_publisher",
@@ -96,4 +99,3 @@ def generate_launch_description():
             rover_base_controller_spawner,
         ]
     )
-
