0e8ecacf8ffd4051df0dcc7fe12f6fa1c82e475f
Sorokonog <roman@voltbro.com>
2026-04-09 00:31:09 +0300

    Harden rover launch helper scripts

--- scripts/start_rover_real.sh
+++ scripts/start_rover_real.sh
@@ -6,8 +6,10 @@ REPO_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 ROS_WS="${ROS_WS:-$(cd "${REPO_DIR}/../.." && pwd)}"
 ROS_DISTRO="${ROS_DISTRO:-jazzy}"
 
+set +u
 source "/opt/ros/${ROS_DISTRO}/setup.bash"
 source "${ROS_WS}/install/setup.bash"
+set -u
 
 exec ros2 launch silverhand_rover_control silverhand_rover_bringup.launch.py \
   use_mock_hardware:=false \
